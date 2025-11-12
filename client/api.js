26d882292e55bd14f23f75daac53ffb8528629b6
MarkoMTP <marko.mtp8@gmail.com>
2025-11-12 10:17:21 +0100

    switched to a railway backend

--- client/api.js
+++ client/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:12345", // 👈 your backend URL
+  baseURL: import.meta.env.VITE_API_URL, // 👈 your backend URL
   headers: {
     "Content-Type": "application/json",
   },
